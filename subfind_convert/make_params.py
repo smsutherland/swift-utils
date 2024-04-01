2d463830ba4987f6ad488e6cfaf381d07ccfd6d8
smsutherland <s.sutherland@uconn.edu>
2024-04-01 13:42:31 -0400

    use correct parameter file

--- subfind_convert/make_params.py
+++ subfind_convert/make_params.py
@@ -1,134 +1,145 @@
 def make_param_file(params):
-    return f"""%% parameters for cosmo_box_star_formation_3d
+    return f"""
+InitCondFile                                      ../../ic.hdf5
+Omega0                                            {params["Omega_m"]}
+OmegaLambda                                       {1-params["Omega_m"]}
+OmegaBaryon                                       {params["Omega_b"]}
+HubbleParam                                       {params["H0"]}
+BoxSize                                           {params["BoxSize"]}
 
-%----  Relevant files
-InitCondFile                            /cosma7/data/dp004/dc-love2/data/CAMELS/ICs/ics_camels_cv_0.hdf5
-OutputDir                               ./
-SnapshotFileBase                        subsnap
-OutputListFilename                      /cosma7/data/dp004/dc-love2/codes/CAMELS-SWIFT/arepo_subfind/output_list_arepo_subfind.txt
+WindEnergyIn1e51erg                               10.3675
+VariableWindVelFactor                             6.76707
+RadioFeedbackFactor                               0.57995
+RadioFeedbackReiorientationFactor                 13.9572
+OutputDir                                         ./
+SnapshotFileBase                                  subsnap
+OutputListFilename                                ../../output_list.txt
+ICFormat                                          3
+SnapFormat                                        3
+TimeLimitCPU                                      398000
+CpuTimeBetRestartFile                             3600
+ResubmitOn                                        0
+ResubmitCommand                                   my-scriptfile
+MaxMemSize                                        17000
+TimeBegin                                         0.0078125
+TimeMax                                           1
+ComovingIntegrationOn                             1
+PeriodicBoundariesOn                              1
+CoolingOn                                         1
+StarformationOn                                   1
+PhotometricsTablePath                             /mnt/home/ssutherland/codes/Arepo_subfind_v2/L75TNG_Arepo_GFM_Tables/Photometrics/
 
-%---- File formats
-ICFormat                                3
-SnapFormat                              3
-
-%---- CPU-time limits
-TimeLimitCPU                            90000
-CpuTimeBetRestartFile                   12000
-% FlushCpuTimeDiff                        120
-
-ResubmitOn                              0
-ResubmitCommand                         my-scriptfile
-
-%----- Memory alloction
-MaxMemSize                              16000
-
-%---- Characteristics of run
-TimeBegin                               0.0078125  % Begin of the simulation; z=127
-TimeMax                                 1.0  % End of the simulation z=0
-
-%---- Basic code options that set the type of simulation
-ComovingIntegrationOn                   1
-PeriodicBoundariesOn                    1
-CoolingOn                               0
-StarformationOn                         0
-
-%---- Cosmological parameters (Planck cosmology)
-Omega0                                  {params["Omega_m"]}
-OmegaLambda                             {1 - params["Omega_m"]}
-OmegaBaryon                             {params["Omega_b"]}
-HubbleParam                             {params["H0"]}
-BoxSize                                 {params["BoxSize"]}
-
-%---- Output frequency and output parameters
-OutputListOn                            1
-TimeBetSnapshot                         0.0
-TimeOfFirstSnapshot                     0.0
-TimeBetStatistics                       0.01
-NumFilesPerSnapshot                     1
-NumFilesWrittenInParallel               1
-
-%---- Accuracy of time integration
-TypeOfTimestepCriterion                 0
-ErrTolIntAccuracy                       0.012
-CourantFac                              0.3
-MaxSizeTimestep                         0.005
-MinSizeTimestep                         1.0e-9
-
-%---- Treatment of empty space and temperature limits
-InitGasTemp                             244.8095
-MinGasTemp                              5.0
-MinimumDensityOnStartUp                 1.0e-20
-LimitUBelowThisDensity                  0.0
-LimitUBelowCertainDensityToThisValue    0.0
-MinEgySpec                              0.0
-
-%---- Tree algorithm, force accuracy, domain update frequency
-TypeOfOpeningCriterion                  1
-ErrTolTheta                             0.7
-ErrTolForceAcc                          0.0025
-MultipleDomains                         8
-TopNodeFactor                           2.5
-ActivePartFracForNewDomainDecomp        0.01
- 
-%---- Initial density estimate
-DesNumNgb                               64
-MaxNumNgbDeviation                      4
-
-%---- System of units
-UnitLength_in_cm                        3.085678e21    %  1.0 kpc
-UnitMass_in_g                           1.989e43       %  1.0e10 solar masses
-UnitVelocity_in_cm_per_s                1e5           %  1 km/sec
-GravityConstantInternal                 0
-
-%---- Gravitational softening lengths
-
-SofteningComovingType0    {params["SofteningComoving"]}
-SofteningComovingType1    {params["SofteningComovingType1"]}
-SofteningComovingType2    {params["SofteningComoving"]}
-SofteningComovingType3    {params["SofteningComoving"]}
-SofteningComovingType4    {params["SofteningComoving"]}
-SofteningComovingType5    {params["SofteningComoving"]}
-
-SofteningMaxPhysType0     {params["SofteningMaxPhys"]}
-SofteningMaxPhysType1     {params["SofteningMaxPhysType1"]}
-SofteningMaxPhysType2     {params["SofteningMaxPhys"]}
-SofteningMaxPhysType3     {params["SofteningMaxPhys"]}
-SofteningMaxPhysType4     {params["SofteningMaxPhys"]}
-SofteningMaxPhysType5     {params["SofteningMaxPhys"]}
-
-SofteningTypeOfPartType0        1
-SofteningTypeOfPartType1        1
-SofteningTypeOfPartType2        1
-SofteningTypeOfPartType3        1
-SofteningTypeOfPartType4        1
-SofteningTypeOfPartType5        2
-
-GasSoftFactor             2.5
-MinimumComovingHydroSoftening   0.25
-AdaptiveHydroSofteningSpacing   1.2
-
-
-%----- Mesh regularization options
-CellShapingSpeed                        0.5
-CellMaxAngleFactor                      2.25
-ReferenceGasPartMass                    0
-TargetGasMassFactor                     1
-RefinementCriterion                     1
-DerefinementCriterion                   1
-
-%----- Subfind
-ErrTolThetaSubfind                      0.7
-DesLinkNgb                              20
-
-%---- Parameters for star formation model
-% CritPhysDensity                         0       % critical physical density for star formation (in cm^(-3))
-% MaxSfrTimescale                         2.27    % in internal time units
-% CritOverDensity                         57.7    % overdensity threshold value
-% TempSupernova                           5.73e7  % in Kelvin
-% TempClouds                              1000.0  % in Kelvin
-% FactorEVP                               573.0
-% TemperatureThresh                       1e+06
-% FactorSN                                0.1
-
-% TreecoolFile                            ./TREECOOL_ep
+OutputListOn                                      1
+TimeBetSnapshot                                   0
+TimeOfFirstSnapshot                               0
+TimeBetStatistics                                 0.01
+NumFilesPerSnapshot                               1
+NumFilesWrittenInParallel                         64
+TypeOfTimestepCriterion                           0
+ErrTolIntAccuracy                                 0.012
+CourantFac                                        0.3
+MaxSizeTimestep                                   0.005
+MinSizeTimestep                                   2e-09
+CritPhysDensity                                   0
+MaxSfrTimescale                                   2.27
+CritOverDensity                                   57.7
+TempSupernova                                     5.73e+07
+TempClouds                                        1000
+FactorEVP                                         573
+TemperatureThresh                                 0
+InitGasTemp                                       170
+MinGasTemp                                        5
+MinimumDensityOnStartUp                           1e-20
+LimitUBelowThisDensity                            0
+LimitUBelowCertainDensityToThisValue              0
+MinEgySpec                                        0
+TypeOfOpeningCriterion                            1
+ErrTolTheta                                       0.7
+ErrTolForceAcc                                    0.0025
+MultipleDomains                                   8
+TopNodeFactor                                     2.5
+ActivePartFracForNewDomainDecomp                  0.01
+DesNumNgb                                         64
+MaxNumNgbDeviation                                4
+UnitLength_in_cm                                  3.08568e+21
+UnitMass_in_g                                     1.989e+43
+UnitVelocity_in_cm_per_s                          100000
+GravityConstantInternal                           0
+SofteningComovingType0                            {params["SofteningComoving"]}
+SofteningComovingType1                            {params["SofteningComovingType1"]}
+SofteningComovingType2                            {params["SofteningComoving"]}
+SofteningComovingType3                            {params["SofteningComoving"]}
+SofteningComovingType4                            {params["SofteningComoving"]}
+SofteningMaxPhysType0                             {params["SofteningMaxPhys"]}
+SofteningMaxPhysType1                             {params["SofteningMaxPhysType1"]}
+SofteningMaxPhysType2                             {params["SofteningMaxPhys"]}
+SofteningMaxPhysType3                             {params["SofteningMaxPhys"]}
+SofteningMaxPhysType4                             {params["SofteningMaxPhys"]}
+SofteningTypeOfPartType0                          0
+SofteningTypeOfPartType1                          1
+SofteningTypeOfPartType2                          1
+SofteningTypeOfPartType3                          1
+SofteningTypeOfPartType4                          1
+SofteningTypeOfPartType5                          2
+GasSoftFactor                                     2.5
+MinimumComovingHydroSoftening                     0.25
+AdaptiveHydroSofteningSpacing                     1.2
+CellShapingSpeed                                  0.5
+CellMaxAngleFactor                                2.25
+ReferenceGasPartMass                              0
+TargetGasMassFactor                               1
+RefinementCriterion                               1
+DerefinementCriterion                             1
+MHDSeedDir                                        4
+MHDSeedValue                                      1e-14
+TreecoolFile                                      /mnt/home/ssutherland/codes/Arepo_subfind_v2/data/TREECOOL_fg_dec11
+ErrTolThetaSubfind                                0.7
+DesLinkNgb                                        20
+IMF_MinMass_Msun                                  0.1
+IMF_MaxMass_Msun                                  100
+AGB_MassTransferOn                                1
+SNIa_MassTransferOn                               1
+SNII_MassTransferOn                               1
+SNII_MinMass_Msun                                 8
+SNII_MaxMass_Msun                                 100
+SNIa_Rate_TAU                                     0.04
+SNIa_Rate_Norm                                    0.0013
+YieldTablePath                                    /mnt/home/ssutherland/codes/Arepo_subfind_v2/L75TNG_Arepo_GFM_Tables/Yields/
+DesNumNgbEnrichment                               64
+MaxNumNgbDeviationEnrichment                      4
+NSNS_MassTransferOn                               1
+NSNS_MassPerEvent                                 0.05
+NSNS_Rate_TAU                                     0.1
+NSNS_per_SNIa                                     0.001
+ThermalWindFraction                               0.1
+VariableWindSpecMomentum                          0
+WindFreeTravelMaxTimeFactor                       0.025
+WindFreeTravelDensFac                             0.05
+TimeBetOnTheFlyFoF                                1.03
+MinWindVel                                        350
+WindEnergyReductionFactor                         0.25
+WindEnergyReductionMetallicity                    0.002
+WindEnergyReductionExponent                       2
+BlackHoleAccretionFactor                          1
+BlackHoleFeedbackFactor                           0.1
+BlackHoleEddingtonFactor                          1
+SeedBlackHoleMass                                 8e-05
+MinFoFMassForNewSeed                              5
+DesNumNgbBlackHole                                128
+BlackHoleMaxAccretionRadius                       1e+20
+BlackHoleRadiativeEfficiency                      0.2
+QuasarThreshold                                   0.002
+RadioFeedbackMinDensityFactor                     0.01
+BlackHoleCenteringMassMultiplier                  1000
+CoolingTablePath                                  /mnt/home/ssutherland/codes/Arepo_subfind_v2/L75TNG_Arepo_GFM_Tables/Cooling/cooling_metal_AGN_Compton_self_shielding_Rahmati12.hdf5
+MinMetalTemp                                      10000
+% PhotometricsTablePath                           /mnt/home/ssutherland/codes/Arepo_subfind_v2/L75TNG_Arepo_GFM_Tables/Photometrics/
+TreecoolFileAGN                                   /mnt/home/ssutherland/codes/Arepo_subfind_v2/data/TREECOOL_AGN
+SelfShieldingDensity                              0.1295
+ObscurationFactor                                 0.3
+ObscurationSlope                                  0.07
+FactorForSofterEQS                                0.3
+TempForSofterEQS                                  10000
+WindDumpFactor                                    0.6
+SelfShieldingFile                                 /mnt/home/ssutherland/codes/Arepo_subfind_v2/data/SelfShielding_Rahmati12
 """
